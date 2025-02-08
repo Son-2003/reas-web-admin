38b9d89dd8b5ff5262dfdfd0441b76ba47892c88
NTDung93 <nguyentiendung2003@gmail.com>
2025-02-08 22:39:33 +0700

    lint fix

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,21 +1,18 @@
 import { AppSidebar } from '@/components/app-sidebar';
-import {
-  SidebarInset,
-  SidebarProvider,
-} from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { Outlet } from 'react-router-dom';
 import AppHeader from './app-header';
 
 export default function AppShell() {
   return (
     <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-      <AppHeader />
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <Outlet />
-      </div>
-    </SidebarInset>
-  </SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <AppHeader />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
