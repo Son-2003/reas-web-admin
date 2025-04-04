6123bce8695869ff86319900bcbe3388341d1c55
NTDung93 <nguyentiendung2003@gmail.com>
2025-04-04 20:27:11 +0700

    beautify login page

--- src/containers/Auth/Login/login-form.tsx
+++ src/containers/Auth/Login/login-form.tsx
@@ -44,13 +44,15 @@ export function LoginForm({
           <form className="p-6 md:p-8" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">{t('welcomeMessage')}</h1>
+                <h1 className="text-2xl font-bold">{t('loginPage.title')}</h1>
                 <p className="text-balance text-muted-foreground">
-                  Login to your Acme Inc account
+                  {t('loginPage.loginMsg')}
                 </p>
               </div>
               <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">
+                  {t('loginPage.emailOrUsername')}
+                </Label>
                 <Input
                   type="text"
                   value={userNameOrEmailOrPhone}
@@ -60,12 +62,14 @@ export function LoginForm({
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+                  <Label htmlFor="password">
+                    {t('loginPage.password')}
+                  </Label>
                   <a
                     href="#"
                     className="ml-auto text-sm underline-offset-2 hover:underline"
                   >
-                    Forgot your password?
+                    {t('loginPage.forgotPassword')}
                   </a>
                 </div>
                 <Input
@@ -78,7 +82,7 @@ export function LoginForm({
                 />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                {t('loginPage.loginButton')}
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
@@ -114,17 +118,11 @@ export function LoginForm({
                   <span className="sr-only">Login with Meta</span>
                 </Button>
               </div>
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{' '}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div>
             </div>
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-              src="src\assets\images\shiba-inu.png"
+              src="https://res.cloudinary.com/dkpg60ca0/image/upload/v1743772817/reas/web-admin/common/gxjmkhebz7bhuu9zoahk.png"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover"
             />
