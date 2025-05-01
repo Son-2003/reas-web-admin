a10a26b79e7a84822bbfd93601af455e4564df79
Nguyễn Đức Sơn <113695548+Son-2003@users.noreply.github.com>
2025-05-01 11:38:00 +0700

    Update index.ts

--- src/utils/environment/index.ts
+++ src/utils/environment/index.ts
@@ -1,4 +1,4 @@
 const BASE_API_URL = import.meta.env.VITE_REACT_BASE_API_URL;
-const WEB_SOCKET_ENDPOINT = 'http://localhost:8080/ws';
+const WEB_SOCKET_ENDPOINT = 'https://api.reasvn.site/ws';
 
 export { BASE_API_URL, WEB_SOCKET_ENDPOINT };
